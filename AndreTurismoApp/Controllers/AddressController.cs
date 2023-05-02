dea2572710daca58310f4426065580e41525a8d9
nicolasbalduino <nicolasbalduino@outlook.com>
2023-05-02 15:25:37 -0300

    feito o Put Address funcionar

--- AndreTurismoApp/Controllers/AddressController.cs
+++ AndreTurismoApp/Controllers/AddressController.cs
@@ -10,10 +10,12 @@ namespace AndreTurismoApp.Controllers
     public class AddressController : ControllerBase
     {
         private AddressService _addressService;
+        private CityService _cityService;
 
         public AddressController()
         {
             _addressService = new AddressService();
+            _cityService = new CityService();
         }
 
         [HttpGet]
@@ -26,7 +28,12 @@ namespace AndreTurismoApp.Controllers
         public async Task<Address> Inserir(Address address) => await _addressService.Insert(address);
 
         [HttpPut("{id}")]
-        public async Task<Address> Atualizar(int id, Address address) => await _addressService.Update(id, address);
+        public async Task<Address> Atualizar(int id, Address address)
+        {
+            var city = await _cityService.FindById(address.City.Id);
+            address.City = city;
+            return await _addressService.Update(id, address);
+        }
 
         [HttpDelete("{id}")]
         public async Task Deletar(int id) => await _addressService.Delete(id);
